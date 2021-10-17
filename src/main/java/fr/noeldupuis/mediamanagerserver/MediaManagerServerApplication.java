f986139704728327127fe667efdc8a952bd8e143
Noel Dupuis <noel.dupuis45370@gmail.com>
2021-10-17 16:05:59 +0200

    Initial Commit

---
+++ src/main/java/fr/noeldupuis/mediamanagerserver/MediaManagerServerApplication.java
@@ -0,0 +1,13 @@
+package fr.noeldupuis.mediamanagerserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MediaManagerServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MediaManagerServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/fr/noeldupuis/mediamanagerserver/MediaManagerServerApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.noeldupuis.mediamanagerserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MediaManagerServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
